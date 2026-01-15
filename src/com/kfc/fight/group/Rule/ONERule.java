58ba79e99d77cc255e028208a736fcce071997a6
kimsebu77 <kimsebu77@gmail.com>
2026-01-15 21:47:31 +0900

    fuckjeho

--- src/com/kfc/fight/group/Rule/ONERule.java
+++ src/com/kfc/fight/group/Rule/ONERule.java
@@ -1,4 +1,39 @@
 package com.kfc.fight.group.Rule;
 
 public class ONERule {
+    public static void kickboxingrule() {
+        System.out.println("ONE 챔피언십 킥복싱 룰");
+        System.out.println("--------------------------------");
+        System.out.println("허용 기술");
+        System.out.println("- 주먹 공격");
+        System.out.println("- 발차기");
+        System.out.println("- 무릎 공격 (짧게만 가능)");
+        System.out.println();
+        System.out.println("금지 기술");
+        System.out.println("- 팔꿈치 공격");
+        System.out.println("- 클린치 지속 공격");
+        System.out.println("- 테이크다운");
+
+        System.out.println("================================");
+        System.out.println("글러브 : 8~10oz");
+        System.out.println("라운드 : 3R (월드 챔피언전 5R)");
+    }
+    public static void muaythairule(){
+        System.out.println("ONE 챔피언십 무에타이 룰");
+        System.out.println("--------------------------------");
+        System.out.println("허용 기술");
+        System.out.println("- 주먹 공격");
+        System.out.println("- 발차기");
+        System.out.println("- 무릎 공격");
+        System.out.println("- 팔꿈치 공격");
+        System.out.println("- 클린치 공격");
+        System.out.println();
+        System.out.println("금지 기술");
+        System.out.println("- 레슬링식 테이크다운");
+        System.out.println("- 장시간 끌고 넘어뜨리기");
+
+        System.out.println("================================");
+        System.out.println("글러브 : MMA 4oz");
+        System.out.println("라운드 : 3R (월드 챔피언전 5R)");
+    }
 }
