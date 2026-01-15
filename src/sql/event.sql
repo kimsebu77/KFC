cbffe3f2731e372ff1cac68bc263aa16a1460d0c
jeho <jeho23ss@gmail.comgit config --global user.email je23ss@naver.com>
2026-01-15 13:42:20 +0900

    sql

--- src/sql/event.sql
+++ src/sql/event.sql
@@ -1,15 +1,24 @@
+create database kfc;
+
+use ufcfighter;
 create table numEvent (
-    num varchar(6) primary key not null,
-    mainCard varchar(30) not null,
-    underCard varchar(30) not null,
+    id int primary key auto_increment,
+    num varchar(20) not null,
     date varchar(20) not null,
     location varchar(50)
 );
 
+create table eventFighter (
+    eventId int,
+    fighterId int,
+    card_type varchar(10)
+);
 
-insert into event values ('UFC324');
-insert into event values ('UFC325');
-insert into event values ('UFC326');
+
+insert into numEvent (num, date, location) values ( 'UFC324', '25.01.26 / 11:00', '미국, 라스베가스');
+insert into numEvent (num, date, location) values ( 'UFC325', '01.02.26 / 11:00', '호주, 시드니 올림픽 공원');
+insert into numEvent (num, date, location) values ( 'UFC326', '08.03.26 / 11:00', '미국, 라스베가스');
 
 
-drop table event
+drop table numEvent;
+drop database kfc;
