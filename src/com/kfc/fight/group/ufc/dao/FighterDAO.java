97e8aae2ac4c85d61b21f62bb55a3668eca7afe7
kimsebu77 <kimsebu77@gmail.com>
2026-01-15 17:25:26 +0900

    fuckjeho

--- src/com/kfc/fight/group/ufc/dao/FighterDAO.java
+++ src/com/kfc/fight/group/ufc/dao/FighterDAO.java
@@ -6,36 +6,38 @@ import com.kfc.fight.group.ufc.model.Fighter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
 public class FighterDAO {
+    String tableName[] = {"fly", "bantam", "light", "feather", "middle", "welter", "lightheavy", "heavy"};
+
     public Fighter findByName(String name) {
-        String sql = "SELECT * FROM fighter WHERE name = ?";
+        for (String table : tableName) {
+            String sql = "SELECT * FROM "+ table +" WHERE name = ?";
 
-        try {
-            Connection conn = DBUtil.getFighterConnection();
-            PreparedStatement ps = conn.prepareStatement(sql);
+            try {
+                Connection conn = DBUtil.getFighterConnection();
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setString(1, name);
+                ResultSet rs = ps.executeQuery();
 
-            ps.setString(1, "%" + name + "%");
-            ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    return new Fighter(
+                            rs.getInt("id"),
+                            rs.getString("name"),
+                            rs.getInt("rich"),
+                            rs.getInt("height"),
+                            rs.getInt("weight"),
+                            rs.getInt("win"),
+                            rs.getInt("lose"),
+                            rs.getDouble("attackaccuracy"),
+                            rs.getDouble("takedownaccuracy"),
+                            rs.getInt("ko_tko"),
+                            rs.getString("fighter_RANK")
+                    );
+                }
 
-            if (rs.next()) {
-                return new Fighter(
-                        rs.getInt("id"),
-                        rs.getString("name"),
-                        rs.getInt("rich"),
-                        rs.getInt("height"),
-                        rs.getInt("weight"),
-                        rs.getInt("win"),
-                        rs.getInt("lose"),
-                        rs.getDouble("attackaccuracy"),
-                        rs.getDouble("takedownaccuracy"),
-                        rs.getInt("ko_tko"),
-                        rs.getInt("fighter_rank")
-                );
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
         }
         return null;
     }
--- src/com/kfc/fight/group/ufc/service/FighterService.java
+++ src/com/kfc/fight/group/ufc/service/FighterService.java
@@ -4,7 +4,7 @@ import com.kfc.fight.group.ufc.dao.FighterDAO;
 import com.kfc.fight.group.ufc.model.Fighter;
 
 public class FighterService {
-    private FighterDAO fighterDAO = new FighterDAO();
+    FighterDAO fighterDAO = new FighterDAO();
 
     public void search(String name) {
 
@@ -17,13 +17,13 @@ public class FighterService {
 
         System.out.println("=== 선수 정보 ===");
         System.out.println("이름: " + fighter.getName() +
-                "\n리치: " + fighter.getRich() +
-                "\n키: " + fighter.getHeight() +
-                "\n몸무게: " +  fighter.getWeight() +
-                "\n승 / 패" + fighter.getWin() + " / " + fighter.getLose() +
-                "\n타격 성공률: " + fighter.getAttackAccuracy() +
-                "\n테이크다운 방어률" + fighter.getTakedownAccuracy() +
-                "\nKO_TKO" + fighter.getKo_tko() +
-                "\n랭크: " + fighter.getFighter_rank());
+                "\n리치 : " + fighter.getRich() +"CM"+
+                "\n키 : " + fighter.getHeight() +"CM"+
+                "\n몸무게 : " +  fighter.getWeight() +"KG"+
+                "\n승/" + fighter.getWin() + " 패/ " + fighter.getLose() +
+                "\n타격 성공률 : " + fighter.getAttackAccuracy() +"%"+
+                "\n테이크다운 방어률 : " + fighter.getTakedownAccuracy() +"%"+
+                "\nKO_TKO : " + fighter.getKo_tko() +"번"+
+                "\n랭크 : " + fighter.getFighter_rank());
     }
 }
