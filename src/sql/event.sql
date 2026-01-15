b25f5829922792e2c4d4c52ca936118344505323
jeho <jeho23ss@gmail.comgit config --global user.email je23ss@naver.com>
2026-01-15 13:56:47 +0900

    sql

--- src/sql/event.sql
+++ src/sql/event.sql
@@ -1,23 +1,45 @@
 create database kfc;
 
 use ufcfighter;
-create table numEvent (
-    id int primary key auto_increment,
-    num varchar(20) not null,
-    date varchar(20) not null,
+
+create table numEvent
+(
+    id       int primary key auto_increment,
+    num      varchar(20) not null,
+    date     varchar(20) not null,
     location varchar(50)
 );
 
-create table eventFighter (
-    eventId int,
+create table eventFighter
+(
+    eventId   int,
     fighterId int,
     card_type varchar(10)
 );
 
+insert into numEvent (num, date, location)
+values ('UFC324', '26.01.26 / 11:00', '미국, 라스베가스');
+insert into numEvent (num, date, location)
+values ('UFC325', '26.02.01 / 11:00', '호주, 시드니 올림픽 공원');
+insert into numEvent (num, date, location)
+values ('UFC326', '26.03.08 / 11:00', '미국, 라스베가스');
+insert into numEvent (num, date, location)
+values ('UFC Fight Night', '26.02.08 / 11:00', '미국, 라스베가스');
+insert into numEvent (num, date, location)
+values ('UFC Fight Night', '26.02.22 / 10:00', '미국, 휴스턴');
+insert into numEvent (num, date, location)
+values ('UFC Fight Night', '26.03.01 / 11:00', '멕시코, 멕시코시티');
 
-insert into numEvent (num, date, location) values ( 'UFC324', '25.01.26 / 11:00', '미국, 라스베가스');
-insert into numEvent (num, date, location) values ( 'UFC325', '01.02.26 / 11:00', '호주, 시드니 올림픽 공원');
-insert into numEvent (num, date, location) values ( 'UFC326', '08.03.26 / 11:00', '미국, 라스베가스');
+delete from numEvent;
+select * from numEvent;
+
+
+insert into eventFighter values ();
+insert into eventFighter values ();
+insert into eventFighter values ();
+insert into eventFighter values ();
+insert into eventFighter values ();
+insert into eventFighter values ();
 
 
 drop table numEvent;
